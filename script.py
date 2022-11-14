829ad53f8a52e4fd70c0f48ef142dccd51b3c6e4
Nick Reiter <nick.reiter@hotmail.de>
2022-11-14 19:15:21 +0100

    added standalone installation of mysql

--- script.py
+++ script.py
@@ -1,16 +1,124 @@
-# This is a sample Python script.
+import boto3
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def createSecurityGroup(ec2_client):
+    """
+        The function creates a new security group in AWS
+        The function retrievs the vsp_id from the AWS portal, as it is personal and needed for creating a new group
+        It then creates the security group using boto3 package
+        then it waits for the creation
+        then it assigns new rules to the security group
+
+        Parameters
+        ----------
+        ec2_client
+            client that allows for sertain functions using boto3
+
+        Returns
+        -------
+        SECURITY_GROUP : list[str]
+            list of the created security group ids
+        vpc_id : str
+            the vpc_id as it is needed for other operations
+
+        Errors
+        -------
+        The function throws an error if a security group with the same name already exists in your AWS
+
+    """
+    # Create security group, using SSH & HHTP access available from anywhere
+    groups = ec2_client.describe_security_groups()
+    vpc_id = groups["SecurityGroups"][0]["VpcId"]
+
+    new_group = ec2_client.create_security_group(
+        Description="SSH and HTTP access",
+        GroupName="Cloud Computing TP2",
+        VpcId=vpc_id
+    )
+
+    # Wait for the security group to exist!
+    new_group_waiter = ec2_client.get_waiter('security_group_exists')
+    new_group_waiter.wait(GroupNames=["Cloud Computing TP2"])
+
+    group_id = new_group["GroupId"]
+
+    rule_creation = ec2_client.authorize_security_group_ingress(
+        GroupName="Cloud Computing TP2",
+        GroupId=group_id,
+        IpPermissions=[{
+            'FromPort': 22,
+            'ToPort': 22,
+            'IpProtocol': 'tcp',
+            'IpRanges': [{'CidrIp': '0.0.0.0/0'}]
+        },
+        {
+            'FromPort': 80,
+            'ToPort': 80,
+            'IpProtocol': 'tcp',
+            'IpRanges': [{'CidrIp': '0.0.0.0/0'}]
+        }]
+    )
+
+    SECURITY_GROUP = [group_id]
+    return SECURITY_GROUP, vpc_id
+
+def getAvailabilityZones(ec2_client):
+    """
+        Retrieving the subnet ids for availability zones
+        they are required to assign for example instances to a specific availabilityzone
+
+        Parameters
+        ----------
+        ec2_client
+            client of boto3 tho access certain methods related to AWS EC2
+
+        Returns
+        -------
+        dict
+            a dictonary, with availability zone name as key and subnet id as value
+
+        """
+    # Availability zones
+    response = ec2_client.describe_subnets()
+
+    availabilityzones = {}
+    for subnet in response.get('Subnets'):
+        # print(subnet)
+        availabilityzones.update({subnet.get('AvailabilityZone'): subnet.get('SubnetId')})
+
+    return availabilityzones
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+
+def main ():
+    """
+        main function to process the apllication
+    :return:
+    """
+
+    """--------------------------------------- Get necesarry clients from boto3 ---------------------------------------"""
+    ec2_client = boto3.client("ec2")
+    ec2 = boto3.resource('ec2')
+
+    """--------------------------------------- Create security group ---------------------------------------"""
+    SECURITY_GROUP, vpc_id = createSecurityGroup(ec2_client)
+    print("security_group: ", SECURITY_GROUP)
+    print("vpc_id: ", str(vpc_id), "\n")
+
+    """--------------------------------------- Get availability Zones ---------------------------------------"""
+    availabilityZones = getAvailabilityZones(ec2_client)
+    print("Availability zones:")
+    print("Zone 1a: ", availabilityZones.get('us-east-1a'), "\n")
+
+    """--------------------------------------- Create the instances ---------------------------------------"""
+    ins_standalone = createInstances(ec2_client, ec2, SECURITY_GROUP, availabilityZones, userdata_hadoop)
+    """--------------------------------------- Install MySQL ---------------------------------------"""
+    """--------------------------------------- ---------------------------------------"""
+    """--------------------------------------- ---------------------------------------"""
+    """--------------------------------------- ---------------------------------------"""
+    """--------------------------------------- ---------------------------------------"""
+    """--------------------------------------- ---------------------------------------"""
+    """--------------------------------------- ---------------------------------------"""
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+main()
