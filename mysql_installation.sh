82a977011294f83ee5b3dcb3b25be15188c46df6
Nick Reiter <nick.reiter@hotmail.de>
2022-11-14 23:17:59 +0100

    added sakila deployment

---
+++ mysql_installation.sh
@@ -0,0 +1,27 @@
+sudo apt-get update
+sudo apt-get install mysql-server
+y| sudo apt install wget
+sudo apt-get install unzip
+sudo mkdir sakila
+cd sakila
+sudo wget https://downloads.mysql.com/docs/sakila-db.zip
+sudo unzip sakila-db.zip
+sudo mysql
+mysql> ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
+mysql> exit
+mysql -u root -p
+password
+mysql> SOURCE ~/sakila/sakila-db/sakila-schema.sql;
+mysql> SOURCE ~/sakila/sakila-db/sakila-data.sql;
+
+
+
+
+'''
+mysql> create database testdb;
+mysql> create user 'testuser'@'localhost' identified by 'password';
+mysql> GRANT ALL PRIVILEGES ON testdb.* TO'testuser'@'localhost';
+mysql> use testdb;
+mysql> create table customers (customer_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, first_name TEXT, last_name TEXT);
+mysql> SHOW COLUMNS FROM customers;
+'''
