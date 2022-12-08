35a31be49096ca938856ce8cf08cb7499605cd5d
Nick Reiter <nick.reiter@hotmail.de>
2022-12-09 00:19:28 +0100

    Finished initialization of slave instances

--- msql_slave.sh
+++ msql_slave.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
+# General
 sudo apt-get update
 yes | sudo apt-get upgrade
 
-# Install MySQL cluster
+# Get MySQL done
 sudo mkdir -p /opt/mysqlcluster/home
 sudo chmod -R 777 /opt/mysqlcluster
-cd /opt/mysqlcluster/home
-wget http://dev.mysql.com/get/Downloads/MySQL-Cluster-7.4/mysql-cluster-gpl-7.4.10-linux-glibc2.5-x86_64.tar.gz
-tar -zxf mysql-cluster-gpl-7.4.10-linux-glibc2.5-x86_64.tar.gz
-ln -s mysql-cluster-gpl-7.4.10-linux-glibc2.5-x86_64 mysqlc
-sudo chmod -R 777 /etc/profile.d
+sudo wget http://dev.mysql.com/get/Downloads/MySQL-Cluster-7.4/mysql-cluster-gpl-7.4.10-linux-glibc2.5-x86_64.tar.gz -P /opt/mysqlcluster/home/
+sudo tar -xvf /opt/mysqlcluster/home/mysql-cluster-gpl-7.4.10-linux-glibc2.5-x86_64.tar.gz -C /opt/mysqlcluster/home/
+sudo ln -s /opt/mysqlcluster/home/mysql-cluster-gpl-7.4.10-linux-glibc2.5-x86_64 /opt/mysqlcluster/home/mysqlc
 
-# add MySQL PATH varibles
+
+# Adjust the path done
+sudo chmod -R 777 /etc/profile.d
 echo "export MYSQLC_HOME=/opt/mysqlcluster/home/mysqlc" > /etc/profile.d/mysqlc.sh
 echo "export PATH=$MYSQLC_HOME/bin:$PATH" >> /etc/profile.d/mysqlc.sh
 source /etc/profile.d/mysqlc.sh
 sudo apt-get update && sudo apt-get -y install libncurses5
 
-# provide master-based information
-sudo mkdir -p /opt/mysqlcluster/deploy/ndb_data
-sudo chmod -R 777 /opt/mysqlcluster/home/mysqlc/bin
-sudo /opt/mysqlcluster/home/mysqlc/bin/ndbd -c ip-172-31-10-58.ec2.internal
+# Install sysbench
+yes | sudo apt-get install sysbench
+mkdir -p /opt/mysqlcluster/deploy/ndb_data
+sudo /opt/mysqlcluster/home/mysqlc/bin/ndbd -c ip-172-31-46-81.ec2.internal
